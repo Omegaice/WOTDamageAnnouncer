c79002d3edfefbd4c92980133c02e332254b6b69
James Sweet <james.sweet88@googlemail.com>
2013-10-14 10:18:43 -0400

    Updated code structure to allow for easier updating for new WOT versions. Closes #36

--- compiler.py
+++ compiler.py
@@ -1,11 +1,13 @@
 import py_compile, zipfile, os
 
+WOTVersion = "0.8.8"
+
 if os.path.exists("ReceivedDamage.zip"):
 	os.remove("ReceivedDamage.zip")
 
-py_compile.compile("res_mods/0.8.8/scripts/client/vehicle.py")
+py_compile.compile("src/vehicle.py")
 
 fZip = zipfile.ZipFile( "ReceivedDamage.zip", "w" )
-fZip.write("res_mods/0.8.8/scripts/client/vehicle.pyc")
-fZip.write("res_mods/0.8.8/scripts/client/vehicle_damage.json")
+fZip.write("src/vehicle.pyc", "res_mods/0.8.8/scripts/client/vehicle.pyc")
+fZip.write("data/vehicle_damage.json", "res_mods/0.8.8/scripts/client/vehicle_damage.json")
 fZip.close()
