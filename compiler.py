addab90b7ef560717e8fbd31e5e5a337fe7d3317
James Sweet <james.sweet88@googlemail.com>
2013-11-01 18:25:00 -0400

    Updated version and made compiler build correct zip file

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 import py_compile, zipfile, os
 
-WOTVersion = "0.8.8"
+WOTVersion = "0.8.9"
 
 if os.path.exists("ReceivedDamage.zip"):
 	os.remove("ReceivedDamage.zip")
@@ -8,6 +8,6 @@ if os.path.exists("ReceivedDamage.zip"):
 py_compile.compile("src/vehicle.py")
 
 fZip = zipfile.ZipFile( "ReceivedDamage.zip", "w" )
-fZip.write("src/vehicle.pyc", "res_mods/0.8.8/scripts/client/vehicle.pyc")
-fZip.write("data/vehicle_damage.json", "res_mods/0.8.8/scripts/client/vehicle_damage.json")
+fZip.write("src/vehicle.pyc", "res_mods/"+WOTVersion+"/scripts/client/vehicle.pyc")
+fZip.write("data/vehicle_damage.json", "res_mods/"+WOTVersion+"/scripts/client/vehicle_damage.json")
 fZip.close()
