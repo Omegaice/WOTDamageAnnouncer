f06e6d3c3e828e681efedbcede06fe2568a03da8
James Sweet <james.sweet88@googlemail.com>
2014-06-11 18:43:05 -0400

    Set this branch's zip file name

--- compiler.py
+++ compiler.py
@@ -2,12 +2,12 @@ import py_compile, zipfile, os
 
 WOTVersion = "0.9.0"
 
-if os.path.exists("ReceivedDamage.zip"):
-	os.remove("ReceivedDamage.zip")
+if os.path.exists("ReceivedDamage-EU.zip"):
+	os.remove("ReceivedDamage-EU.zip")
 
 py_compile.compile("src/vehicle.py")
 
-fZip = zipfile.ZipFile( "ReceivedDamage.zip", "w" )
+fZip = zipfile.ZipFile( "ReceivedDamage-EU.zip", "w" )
 fZip.write("src/vehicle.pyc", "res_mods/"+WOTVersion+"/scripts/client/vehicle.pyc")
 fZip.write("data/vehicle_damage.json", "res_mods/"+WOTVersion+"/scripts/client/vehicle_damage.json")
 fZip.close()
