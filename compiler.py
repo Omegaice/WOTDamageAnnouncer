7ac6a61bc488d588ded85d705e4c91597fae56d2
James Sweet <james.sweet88@googlemail.com>
2014-06-12 10:19:20 -0400

    Added obfuscation

--- compiler.py
+++ compiler.py
@@ -1,13 +1,24 @@
-import py_compile, zipfile, os
+import py_compile, zipfile, os, subprocess
+import shlex
 
 WOTVersion = "0.9.1"
+ZIPName = "ReceivedDamage-NA.zip"
 
-if os.path.exists("ReceivedDamage.zip"):
-	os.remove("ReceivedDamage.zip")
+if os.path.exists( ZIPName ):
+	os.remove( ZIPName )
 
-py_compile.compile("src/vehicle.py")
+p = subprocess.Popen(shlex.split("pyobfuscate -a src/vehicle.py"), stdout=subprocess.PIPE)
+(output, err) = p.communicate()
 
-fZip = zipfile.ZipFile( "ReceivedDamage.zip", "w" )
-fZip.write("src/vehicle.pyc", "res_mods/"+WOTVersion+"/scripts/client/vehicle.pyc")
+with open("src/vehicle_obs.py", "w") as text_file:
+	text_file.write(output)
+
+py_compile.compile("src/vehicle_obs.py")
+
+fZip = zipfile.ZipFile( ZIPName, "w" )
+fZip.write("src/vehicle_obs.pyc", "res_mods/"+WOTVersion+"/scripts/client/vehicle.pyc")
 fZip.write("data/vehicle_damage.json", "res_mods/"+WOTVersion+"/scripts/client/vehicle_damage.json")
 fZip.close()
+
+if os.path.exists( "src/vehicle_obs.py" ):
+	os.remove( "src/vehicle_obs.py" )
